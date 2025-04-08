3e0b09fa9b529f47b9642b6afdb8b2ab7d843581
giraldi07 <giraldipramay@gmail.com>
2025-04-08 17:48:28 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.png'],
       manifest: {
         name: 'Smart Indo TV',
         short_name: 'IndoTV',
@@ -18,12 +18,12 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'favicon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'favicon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
@@ -33,7 +33,7 @@ export default defineConfig({
         categories: ['entertainment', 'video', 'streaming'],
         screenshots: [
           {
-            src: 'screenshot1.png',
+            src: 'television.png',
             sizes: '1170x2532',
             type: 'image/png'
           }
